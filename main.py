32ddc528fc1e039751871daa0db85e033e3d4b92
ChzenChzen <fedorovdanila@gmail.com>
2017-04-01 11:03:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def h2n_text_parser(filename):
+    """parses text from file"""
+    lines = [[int(weight) for weight in line.split()] for line in open(filename)]  # create hand matrix from file
+    return lines
+
+def convert_into_percents():
+    """converts weights of raw h2n into percents"""
+    pass
+
+
+def convert_into_crev_format():
+    """rounds weights of percents  into crev format(5 weights)"""
+    pass
+
+
+def write_crev_format_in_excel():
+    """writes crev weighted data in excel's table for handy usage"""
+    pass
+
+
+print(h2n_text_parser("input.txt"))
