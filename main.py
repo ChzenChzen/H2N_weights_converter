3501c9f8d3eea1fa33ad37ffd0f37fd981800840
ChzenChzen <fedorovdanila@gmail.com>
2017-04-01 17:32:23 +0300

    Добавил функцию добавления разномастных карт в матрицу карт

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
+import pandas as pd
+
 def h2n_text_parser(filename):
     """parses text from file"""
     weights = [[int(weight) for weight in line.split()] for line in open(filename)]  # create hand matrix from file
     return weights
 
 
-def offsuit_cards(cards_matrix):
+def get_offsuit_cards(cards_matrix):
     """returns list of offsuit cards"""
     offsuits = []
     offsuit_cards_length = 0
     # take only values of offsuit cards from matrix
-    for cards_matrix_line in cards_matrix[1:]:
+    for cards_matrix_row in cards_matrix[1:]:
         offsuit_cards_length += 1
-        for card_weight in cards_matrix_line[:offsuit_cards_length]:
+        for card_weight in cards_matrix_row[:offsuit_cards_length]:
             offsuits.append(card_weight)
     return offsuits
 
 
-def suit_cards(cards_matrix):
+def get_suit_cards(cards_matrix):
     """returns list of suit cards"""
     suits = []
     suit_cards_length = 0
     # take only values of suit cards from matrix
-    for cards_matrix_line in cards_matrix[:-1]:
+    for cards_matrix_row in cards_matrix[:-1]:
         suit_cards_length += 1
-        for card_weight in cards_matrix_line[suit_cards_length:]:
+        for card_weight in cards_matrix_row[suit_cards_length:]:
             suits.append(card_weight)
     return suits
 
 
-def pair_cards(cards_matrix):
+def get_pair_cards(cards_matrix):
     """returns list of pair cards"""
     pairs = [cards_matrix[i][i] for i in range(len(cards_matrix))]  # take only values of pair cards from matrix
     return pairs
@@ -39,6 +41,13 @@ def convert_into_percents(cards_range):
     max_cards_range_value = max(cards_range)
     percent_values = [round(card_value / max_cards_range_value, 2) for card_value in cards_range]
     return percent_values
+
+
+def add_offsuit_cards(offsuit_values, cards_matrix):
+    """add offsuit cards in empty cards matrix"""
+    for n in range(13):
+        cards_matrix.append(offsuit_values[:n])
+        del offsuit_values[:n]
 
 
 def convert_into_crev_format():
@@ -52,7 +61,11 @@ def write_crev_format_in_excel():
 
 
 weights = h2n_text_parser("input.txt")
+matrix = []
 
-print(convert_into_percents(suit_cards(weights)))
-print(convert_into_percents(offsuit_cards(weights)))
-print(convert_into_percents(pair_cards(weights)))
+offsuit_cards_in_percent = convert_into_percents(get_offsuit_cards(weights))
+# print(convert_into_percents(pair_cards(weights)))
+# print(convert_into_percents(suit_cards(weights)))
+
+add_offsuit_cards(offsuit_cards_in_percent, matrix)
+print(matrix)
