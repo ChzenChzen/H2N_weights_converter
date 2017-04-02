60470e501320263e3446034fefc4bfd526d94a7f
ChzenChzen <fedorovdanila@gmail.com>
2017-04-02 22:15:20 +0300

    понял что представление в виде матрицы лишнее и перешел к использованию словарей

--- main.py
+++ main.py
@@ -49,29 +49,28 @@ def show_matrix(matrix):
         print(str(row) + "\n")
 
 
-def add_offsuit_cards_percents(offsuit_values, cards_matrix):
-    """add offsuit cards in cards matrix"""
-    n = 0
-    for row in cards_matrix:
-        row[:n] = offsuit_values[:n]
-        del offsuit_values[:n]
-        n += 1
-
-
-def add_pair_cards(pair_values, cards_matrix):
-    """add pair cards in cards matrix"""
-    for n in range(13):
-        cards_matrix[n][n] = pair_values[n]
-
-
-def add_suit_cards_percents(suit_values, cards_matrix):
-    """add suit cards in cards matrix"""
-    n = 1
-    for row in cards_matrix:
-        row[n:] = suit_values[:13 - n]
-        del suit_values[:13 - n]
-        n += 1
-
+# def add_offsuit_cards_percents(offsuit_values, cards_matrix):
+#     """add offsuit cards in cards matrix"""
+#     n = 0
+#     for row in cards_matrix:
+#         row[:n] = offsuit_values[:n]
+#         del offsuit_values[:n]
+#         n += 1
+#
+#
+# def add_pair_cards(pair_values, cards_matrix):
+#     """add pair cards in cards matrix"""
+#     for n in range(13):
+#         cards_matrix[n][n] = pair_values[n]
+#
+#
+# def add_suit_cards_percents(suit_values, cards_matrix):
+#     """add suit cards in cards matrix"""
+#     n = 1
+#     for row in cards_matrix:
+#         row[n:] = suit_values[:13 - n]
+#         del suit_values[:13 - n]
+#         n += 1
 
 def convert_into_crev_format():
     """rounds weights of percents  into crev format(5 weights)"""
@@ -83,15 +82,79 @@ def write_crev_format_in_excel():
     pass
 
 
+def pair_cards(values):
+    """does dict of pair cards with value"""
+    pair_cards_val_in_percent = convert_into_percents(get_pair_cards(values))  # list of pair cards
+    pair_card_labels = ['AA', 'KK', 'QQ', 'JJ', 'TT', '99', '88', '77', '66', '55', '44', '33', '22']
+    pair_cards_dict = {}
+    for index, pair_card_label in enumerate(pair_card_labels):
+        pair_cards_dict[pair_card_label] = pair_cards_val_in_percent[index]
+    return pair_cards_dict
+
+
+def offsuit_cards(values):
+    """does dict of pair cards with value"""
+    offsuit_cards_val_in_percent = convert_into_percents(get_offsuit_cards(values))  # list of pair cards
+    offsuit_card_labels = ['AKo',
+                           'AQo', 'KQo',
+                           'AJo', 'KJo', 'QJo',
+                           'ATo', 'KTo', 'QTo', 'JTo',
+                           'A9o', 'K9o', 'Q9o', 'J9o', 'T9o',
+                           'A8o', 'K8o', 'Q8o', 'J8o', 'T8o', '98o',
+                           'A7o', 'K7o', 'Q7o', 'J7o', 'T7o', '97o', '87o',
+                           'A6o', 'K6o', 'Q6o', 'J6o', 'T6o', '96o', '86o', '76o',
+                           'A5o', 'K5o', 'Q5o', 'J5o', 'T5o', '95o', '85o', '75o', '65o',
+                           'A4o', 'K4o', 'Q4o', 'J4o', 'T4o', '94o', '84o', '74o', '64o', '54o',
+                           'A3o', 'K3o', 'Q3o', 'J3o', 'T3o', '93o', '83o', '73o', '63o', '53o', '43o',
+                           'A2o', 'K2o', 'Q2o', 'J2o', 'T2o', '92o', '82o', '72o', '62o', '52o', '42o', '32o']
+    offsuit_cards_dict = {}
+    for index, offsuit_card_label in enumerate(offsuit_card_labels):
+        offsuit_cards_dict[offsuit_card_label] = offsuit_cards_val_in_percent[index]
+    return offsuit_cards_dict
+
+
+def suit_cards(values):
+    """does dict of pair cards with value"""
+    suit_cards_val_in_percent = convert_into_percents(get_suit_cards(values))  # list of pair cards
+    suit_card_labels = ['AKs', 'AQs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'A5s', 'A4s', 'A3s', 'A2s',
+                        'KQs', 'KJs', 'KTs', 'K9s', 'K8s', 'K7s', 'K6s', 'K5s', 'K4s', 'K3s', 'K2s',
+                        'QJs', 'QTs', 'Q9s', 'Q8s', 'Q7s', 'Q6s', 'Q5s', 'Q4s', 'Q3s', 'Q2s',
+                        'JTs', 'J9s', 'J8s', 'J7s', 'J6s', 'J5s', 'J4s', 'J3s', 'J2s',
+                        'T9s', 'T8s', 'T7s', 'T6s', 'T5s', 'T4s', 'T3s', 'T2s',
+                        '98s', '97s', '96s', '95s', '94s', '93s', '92s',
+                        '87s', '86s', '85s', '84s', '83s', '82s',
+                        '76s', '75s', '74s', '73s', '72s',
+                        '65s', '64s', '63s', '62s',
+                        '54s', '53s', '52s',
+                        '43s', '42s',
+                        '32s']
+    suit_cards_dict = {}
+    for index, suit_card_label in enumerate(suit_card_labels):
+        suit_cards_dict[suit_card_label] = suit_cards_val_in_percent[index]
+    return suit_cards_dict
+
 data = h2n_text_parser("input.txt")
-matrix = [['' for x in range(13)] for x in range(13)]  # create empty matrix 13*13
-offsuit_cards_in_percent = convert_into_percents(get_offsuit_cards(data))
-pair_cards_in_percent = convert_into_percents(get_pair_cards(data))
-suit_cards_in_percent = convert_into_percents(get_suit_cards(data))
+# matrix = [['' for x in range(13)] for x in range(13)]  # create empty matrix 13*13
+
+all_cards = dict(**pair_cards(data), **offsuit_cards(data), **suit_cards(data))
+print(all_cards)
+# print(all_cards(data))
+
+# for index,
 
 
-add_offsuit_cards_percents(offsuit_cards_in_percent, matrix)
-add_pair_cards(pair_cards_in_percent, matrix)
-add_suit_cards_percents(suit_cards_in_percent, matrix)
-print("###################################################################")
-show_matrix(matrix)
+
+# add_offsuit_cards_percents(offsuit_cards_in_percent, matrix)
+# add_pair_cards(pair_cards_in_percent, matrix)
+# add_suit_cards_percents(suit_cards_in_percent, matrix)
+#
+# card_labels = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+#
+# df = pd.DataFrame(matrix, columns=card_labels, index=card_labels)
+# print(df)
+#
+# for row in card_labels:
+#     for card in card_labels:
+#         value = df.at[row, card]
+#         if value < 0.2:
+#             print(row, card, value)
